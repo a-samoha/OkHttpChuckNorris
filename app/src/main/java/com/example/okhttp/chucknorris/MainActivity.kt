b3e99f9cf9953cac7a8b0b2f0d00a75e360ec35c
samoshkinr-tem <samoshkin-@ukr.net>
2021-10-11 14:11:36 +0300

    this is code from https://habr.com/ru/post/450488/

--- app/src/main/java/com/example/okhttp/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/okhttp/chucknorris/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.okhttp.chucknorris
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.okhttp.chucknorris.databinding.ActivityMainBinding
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    val URL = "https://api.icndb.com/jokes/random"
+    var okHttpClient: OkHttpClient = OkHttpClient()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextBtn.setOnClickListener {
+            loadRandomFact()
+        }
+    }
+
+    private fun loadRandomFact() {
+        binding.progressBar.visibility = View.VISIBLE
+
+        val request: Request = Request.Builder().url(URL).build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val json = response?.body()?.string()
+                val txt = (JSONObject(json).getJSONObject("value").get("joke")).toString()
+
+                runOnUiThread {
+                    binding.progressBar.visibility = View.GONE
+                    binding.factTv.text = Html.fromHtml(txt)
+                }
+            }
+        })
     }
 }
